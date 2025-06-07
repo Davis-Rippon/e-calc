ca0cc0dc19ea4339ecada8720d21f3ecad0d9802
Davis-Rippon <drip0002@student.monash.edu>
2025-06-07 21:51:46 +1000

    add new functions

--- src/functions.py
+++ src/functions.py
@@ -2,13 +2,54 @@ import math
 from .complex import Complex
 
 
-def toCart(amplitude:float = 1, sinusoid:str="cos", phaseDeg:float=0):
+def toCart(amplitude:Complex = Complex(1), sinusoid:str="cos", phaseDeg:Complex=Complex(0)):
     if sinusoid == "sin": phaseDeg -= 90 # convert to cos if sin
-    phaseDeg = math.radians(phaseDeg) # convert to radians
-    return Complex(amplitude*math.cos(phaseDeg), amplitude*math.sin(phaseDeg))
+    amp = float(amplitude.real)
+    phdeg = math.radians(phaseDeg.real) # convert to radians
+    return Complex(amp*math.cos(phdeg), amp*math.sin(phdeg))
 
-def vdiv(r1, r2, v):
-    return v*(r2/r1 + r2)
+def toPhasor(v:Complex= Complex(0,0)):
+    """
+    Prints Phasor representation of complex number
+
+    v: complex numbre
+    returns: None
+    """
+    im = v.imag
+    re = v.real
+
+    mag = math.hypot(im, re)
+    angle = math.atan2(im, re)*180/math.pi
+
+    # if angle > 90:
+    #     angle -= 90
+    #
+    # elif angle <= -90:
+    #     angle += 90
+
+    print(f"Polar Form: {mag} ∠ {angle}°")
+    return v
+
+
+def vdiv(v, r1, r2):
+    """
+    r1: resistance
+    r2: resistance
+    returns: voltage across r1
+    """
+    return v*(r1/(r1 + r2))
+
+def cdiv(c, r1, r2):
+    """
+    r1: resistance
+    r2: resistance
+    returns: current across r1
+    """
+    return c*(r2/(r1 + r2))
 
 FUNCTIONS_TABLE = [("toCart", toCart), 
-                   ("vdiv", vdiv)]
+                   ("vdiv", vdiv),
+                   ("toPhasor", toPhasor),
+                   ("cdiv", cdiv),
+                   ("cos", math.cos),
+                   ("sin", math.sin)]
